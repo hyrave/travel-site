d1bc42153e357486d3b2c8423556c94b1de33ab0
hyrave <hyrave@gmail.com>
2020-07-18 18:25:17 +0800

    done with section4

--- server.js
+++ server.js
@@ -6,11 +6,27 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const path = __dirname + '/app/';
+
 //App
 const app = express();
-app.get('/', (req, res) => {
-   res.send('Hello World');
+const router = express.Router();
+
+router.use(function (req,res,next) {
+   console.log('/' + req.method);
+   next();
 });
+
+router.get('/', (req, res) => {
+   res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req,res) {
+   res.sendFile(path +'sharks.html');
+})
+
+app.use(express.static(path));
+app.use('/', router);
 
 app.listen(PORT, HOST);
 console.log('Running on https://'+HOST+':'+PORT);
